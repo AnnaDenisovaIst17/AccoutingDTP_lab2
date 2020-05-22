5a89e41256515ccd0a994b88735b8943d833fa06
AnnaDenisovaIst17 <denisovaa.a@mail.ru>
2020-05-22 12:07:36 +0300

    Проба Git

--- AccountingDTP/Controllers/HomeController.cs
+++ AccountingDTP/Controllers/HomeController.cs
@@ -17,7 +17,8 @@ namespace AccountingDTP.Controllers
 
         public IActionResult About()
         {
-            ViewData["Message"] = "Your application description page.";
+            ViewData["Message"] = "Your application description page.!!!";
+
 
             return View();
         }
